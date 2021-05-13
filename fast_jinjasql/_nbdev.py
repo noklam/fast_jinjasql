0e408f4cbac9ede6541bbc0c5946ac53c50a7154
noklam <channoklam@yahoo.com.hk>
2021-05-13 23:15:54 +0800

    update

--- fast_jinjasql/_nbdev.py
+++ fast_jinjasql/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"say_hello": "00_core.ipynb"}
+index = {"quote_sql_string": "00_core.ipynb",
+         "get_sql_from_template": "00_core.ipynb"}
 
 modules = ["core.py"]
 
